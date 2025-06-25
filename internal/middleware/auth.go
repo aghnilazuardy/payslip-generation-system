bf75a30b5a8b019ca3f3ebb6f97bf3972b257439
aghni <aghni@premiro.com>
2025-06-25 23:14:38 +0700

    authentication middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,49 +1,53 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 	"payslip-generation-system/utils"
 	"strings"
-
-	"github.com/gin-gonic/gin"
 )
 
-func AuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		if !strings.HasPrefix(authHeader, "Bearer ") {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing or malformed token"})
+type key string
+
+const (
+	UserIDKey key = "user_id"
+	RoleKey   key = "role"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "missing or malformed token", http.StatusUnauthorized)
 			return
 		}
 
 		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
 		userID, role, err := utils.ParseToken(tokenStr)
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
 		// Store in context
-		c.Set("user_id", userID)
-		c.Set("role", role)
-		c.Next()
-	}
+		ctx := context.WithValue(r.Context(), UserIDKey, userID)
+		ctx = context.WithValue(ctx, RoleKey, role)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
-func GetUserID(c *gin.Context) string {
-	if val, exists := c.Get("user_id"); exists {
-		if s, ok := val.(string); ok {
-			return s
-		}
+func GetUserID(r *http.Request) string {
+	val := r.Context().Value(UserIDKey)
+	if s, ok := val.(string); ok {
+		return s
 	}
 	return ""
 }
 
-func GetUserRole(c *gin.Context) string {
-	if val, exists := c.Get("role"); exists {
-		if s, ok := val.(string); ok {
-			return s
-		}
+func GetUserRole(r *http.Request) string {
+	val := r.Context().Value(RoleKey)
+	if s, ok := val.(string); ok {
+		return s
 	}
 	return ""
 }
